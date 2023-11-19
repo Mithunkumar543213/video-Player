632d55a79fb0db2c1ef2d6ca61fb52c077b39ba8
unknown <mithunkumaryaduvansy123@gmail.com>
2023-11-19 22:38:14 +0530

    database connection

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    let connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(`mongooDB connected ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("ERROR :", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// require('dotenv').config({path:"./env"})
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+import  express  from "express";
+const app=express();
+ 
+dotenv.config({
+    path:"./env"
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`app is listening on port ${process.env.PORT}`);
+})
+
+connectDB()
