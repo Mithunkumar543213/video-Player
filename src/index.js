2d56276eb64aa385d51d850f8f440a16cb149a80
Mithun Kumar Yadav <mithunkumaryaduvansy123@gmail.com>
2024-01-14 22:27:51 +0530

    prettier config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 // require('dotenv').config({path:"./env"})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
-import  express  from "express";
-const app=express();
- 
-dotenv.config({
-    path:"./env"
-})
+import express from "express";
+const app = express();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`app is listening on port ${process.env.PORT}`);
-})
+dotenv.config({
+  path: "./env",
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`app is listening on port ${process.env.PORT}`);
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`app is listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`dataBaseConnectionError:${error}`);
+  });
