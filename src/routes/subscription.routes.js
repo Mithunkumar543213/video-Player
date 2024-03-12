0cc9b090d8aa726efa9bb62ea36217b565c7340c
Mithun Kumar Yadav <mithunkumaryaduvansy123@gmail.com>
2024-03-13 01:45:57 +0530

    tweet and subscription routes

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import{getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription} from "../controllers/subscription.controller.js" 
+import { verifyjwt } from "../middlewares/loggedOut.js";
+
+const router = Router();
+router.use(verifyjwt); // Apply verifyJWT middleware to all routes in this file
+
+router
+    .route("/c/:channelId")
+    .get(getSubscribedChannels)
+    .post(toggleSubscription);
+
+router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+
+export default router
---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { createTweet,
+    deleteTweet,
+    getUserTweets,
+    updateTweet,} from "../controllers/tweet.controller.js"
+import { verifyjwt } from "../middlewares/loggedOut.js";
+
+const router = Router();
+router.use(verifyjwt); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/").post(createTweet);
+router.route("/user/:userId").get(getUserTweets);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+
+export default router
