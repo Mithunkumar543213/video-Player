bf78360b62075a87273e5bd85b4030dd68aecc59
Mithun Kumar Yadav <mithunkumaryaduvansy123@gmail.com>
2024-09-25 20:13:16 +0530

    checking some error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,13 +6,13 @@ import  jwt  from "jsonwebtoken";
 
 export const verifyJWT =asyncHandler(async(req,_,next)=>{
 try {
-    const token=req.cookies?.accessToken  || req.header("Authorization")?.replace("Bearer " ,"")
+    const token=req.cookies?.accessToken  || req.header("Authorization")?.replace("Bearer" ,"")
 
     if(!token){
         throw  new ApiError(401,"Unauthorized request")
     }
 
-    const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET) 
 
     const user = await User.findById(decodedToken._id).select("-password -refreshToken")
 
