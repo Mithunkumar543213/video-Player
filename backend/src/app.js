a228106bf87775e418c8eddaf19522801236ab19
Mithun Kumar Yadav <mithunkumaryaduvansy123@gmail.com>
2025-03-30 13:46:25 +0530

    fix: update CORS origin to remove trailing slash

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));   //to serve static folder data
 app.use(cookieParser());     //to set or excess user browser cookie
 
 app.use(cors({
-    origin:"https://videotube-sage.vercel.app/",
+    origin:"https://videotube-sage.vercel.app",
     credentials: true,
     })
 )
