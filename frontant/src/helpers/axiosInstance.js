38d1ce97c60e4c582250baaa792bdf9ec6f07dbd
Mithun Kumar Yadav <mithunkumaryaduvansy123@gmail.com>
2025-03-30 17:40:58 +0530

    some chages in frontant

--- frontant/src/helpers/axiosInstance.js
+++ frontant/src/helpers/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import {BASE_URL} from "../../constants.js";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,  // ✅ Required for sending cookies in cross-origin requests
+});
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
